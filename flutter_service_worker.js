cd1ccc06a8321027f945e639d8d2d81d9affa95f
markmyanswer <markmyanswer@gmail.com>
2024-06-22 17:16:47 +0100

    Deploy Flutter web app with correct base href

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,8 +23,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c8f43546b73db9a49d9f219fd8d64a1f",
-"/": "c8f43546b73db9a49d9f219fd8d64a1f",
+"index.html": "dff94ffe56cb6feaddc23a04816b60c7",
+"/": "dff94ffe56cb6feaddc23a04816b60c7",
 "main.dart.js": "f2dc60845c99c56016ee801a8788790e",
 "manifest.json": "6904f3a27b5d76886ebb5cda5d2124cd",
 "version.json": "c507cbec84a67e9c7a99de0a70f97de9"};
